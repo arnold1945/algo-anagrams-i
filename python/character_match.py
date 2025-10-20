5d4c967c2b856c53a89b08268e1450153d40df50
arnold1945 <vscj58@gmail.com>
2025-10-19 22:15:27 -0500

    done

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,13 @@
 # Don't forget to run your tests!
 
+
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+    
+    
+    for char in string1:
+        if char.lower() in string2.lower():
+            return True
+        else:
+            False
+
+        
