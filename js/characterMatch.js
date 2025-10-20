c0e3104112245c9f111e0458034216d47dc858ad
arnold1945 <vscj58@gmail.com>
2025-10-19 22:35:08 -0500

    js

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,3 +1,11 @@
-exports.isCharacterMatch = function(string1, string2) {
+exports.isCharacterMatch = function(string1,string2) {
 
+    for (let char of string1){
+        if (string2.toLowerCase().includes(char.toLowerCase())){
+            return true
+        }
+    }
+    return false
 };
+
+
